cf871f4b7aede08c8e0925a1aab895efe73445fd
Dima <dmitrijzhuchenko2008@gmail.com>
2024-11-04 20:11:10 +0600

    путь для svg

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
         <div className="d-flex align-center justify-between mb-40">
           <h1>Все кроссовки</h1>
           <div className="search-block d-flex">
-            <img src="/search.svg" alt="Search" />
+            <img src="/img/search.svg" alt="Search" />
             <input placeholder="Поиск..." />
           </div>
         </div>
